2b057e09f6e0f72519cccd61e15ae421dcad09a1
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-10 09:55:26 -0800

    testing again

--- src/components/PokemonList/PokemonList.test.js
+++ src/components/PokemonList/PokemonList.test.js
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import PokemonList from './PokemonList';
 
-test('renders the pokemon speed', () => {
+test('renders the pokemon name', () => {
   const container = render(<PokemonList pokemon={['abra']} />);
   expect(container).toMatchSnapshot();
 });
